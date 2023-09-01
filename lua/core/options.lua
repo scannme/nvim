bfabdafd3ec08bf8e39e9e5620dafef7cd40e325
scannme <lit468@163.com>
2023-08-31 19:51:55 -0700

    init vim

---
+++ lua/core/options.lua
@@ -0,0 +1,23 @@
+local opt = vim.opt
+
+-- 行号
+opt.number = true
+opt.relativenumber = true
+
+-- 缩进
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+-- 默认新窗口
+opt.splitright = true
+opt.splitbelow = true
+
+-- 搜索
+opt.ignorecase = true
+opt.smartcase = true
+
+-- 外观
+opt.termguicolors = true
+opt.signcolumn = "yes"
+
